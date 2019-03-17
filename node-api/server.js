82dd271304935e8ae3400c0427109fe3aa568b8b
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-03-17 15:55:16 -0300

    Mongoose

--- node-api/server.js
+++ node-api/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+//Iniciando o App
 const app = express();
 
-//Creating routes
+//Criando sessão iniciando banco de dados
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true }); //Talvez seja necessário remover esse parâmetro, dependendo da versão do mongoose
+
+//Criando primeira rota
 app.get('/', (req, res) => {
     res.send('Hello João');
 });
